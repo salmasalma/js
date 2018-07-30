c8e4770f1ffcc5cf637e946445336a40779c4531
salmasalma <41632577+salmasalma@users.noreply.github.com>
2018-07-30 12:11:29 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function f1() { document.querySelector('#txt').style.fontWeight="bold"};
+function f2() { document.querySelector('#txt').style.fontStyle="italic"};
+function f3() { document.querySelector('#txt').style.textDecoration = "underline"};
+function a() { 
+    document.querySelector('#txt').style.fontFamily = document.getElementById("font").value;
+}
+function z() { 
+    document.querySelector('#txt').style.fontSize = document.getElementById("Size").value;
+}
